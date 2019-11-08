875c2d0a5d8b971353b30b748b2c1fe4c25ec758
Damion de Wit <damion.dewit@outlook.com>
2019-11-08 20:29:46 +0100

    Added release-log component

---
+++ src/app/release-log/release-log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-release-log',
+  templateUrl: './release-log.component.html',
+  styleUrls: ['./release-log.component.scss']
+})
+export class ReleaseLogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
