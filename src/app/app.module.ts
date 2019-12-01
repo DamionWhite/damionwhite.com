772c55c0e20b575cfa15bd3a0cd4fb10d1a0e70a
Damion de Wit <damion.dewit@outlook.com>
2019-12-01 14:23:20 +0100

    Renamed change-log -> changelog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChangeLogComponent } from './change-log/change-log.component';
+import { ChangeLogComponent } from './changelog/changelog.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/changelog/changelog.component.spec.ts
+++ src/app/changelog/changelog.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ChangeLogComponent } from './change-log.component';
+import { ChangeLogComponent } from './changelog.component';
 
 describe('ReleaseLogComponent', () => {
   let component: ChangeLogComponent;
