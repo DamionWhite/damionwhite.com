54860f539c1d517afd806ced5a3b62fca46aed65
Damion White <damion.dewit@outlook.com>
2019-12-12 16:07:42 +0100

    changes of same type grouped together

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChangelogService, ChangeType, Change } from '../services/changelog.service';
 import { Version } from '../version';
 import { NgModel } from '@angular/forms';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-changelog',
@@ -9,6 +10,8 @@ import { NgModel } from '@angular/forms';
   styleUrls: ['./changelog.component.scss']
 })
 export class ChangeLogComponent implements OnInit {
+  public ChangeType = ChangeType;   // Make enum accessable to template
+
   public changelog: Change[] = [];
 
   public versions: string[] = [];
@@ -23,14 +26,18 @@ export class ChangeLogComponent implements OnInit {
     });
 
     this.changelogService.addChange({
-      type: ChangeType.Change, message: 'Using \'Changelog\' over \'Releaselog\'', version: new Version(0, 1, 1)
+      type: ChangeType.Change, message: 'Using \'Changelog\' over \'Releaselog\'', version: new Version(0, 1, 0)
     });
 
     this.changelogService.addChange({
       type: ChangeType.Removed, message: 'Removed releaselog', version: new Version(0, 1, 0)
     });
 
-    this.changelog = this.changelogService.getVersionChangelog('0.1.1');
+    this.changelogService.addChange({
+      type: ChangeType.Added, message: 'Added more changes', version: new Version(0, 1, 0)
+    });
+
+    this.changelog = this.changelogService.getVersionChangelog('0.1.0');
 
     console.log(this.versions = this.changelogService.getVersions());
 
@@ -38,14 +45,28 @@ export class ChangeLogComponent implements OnInit {
     // End Testing
 
     console.log(this.selectedVersion = this.changelogService.getVersions()[0]);
+
+
   }
 
+  // Triggers when a new version is selected
   public onVersionSelect(event: Event) {
     // Set Version
     this.selectedVersion = (event.target as HTMLSelectElement).value;
 
     // Get Changes for version
     this.getSelectedVersionChanges();
+  }
+
+  // Checks if changeType is present in the currently selected version
+  // Returns true if changeType is present, else returns false
+  public isChangeTypePresent(changeType: ChangeType): boolean {
+    return Boolean(this.changelog.find(change => change.getType() === changeType ));
+  }
+
+  // Returns an array of changes based on changetype
+  public getChangesByType(changeType: ChangeType): Change[] {
+    return this.changelog.filter(change => change.getType() === changeType);
   }
 
   private getSelectedVersionChanges(): void {
